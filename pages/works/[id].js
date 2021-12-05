87530bb06b90a27542c1f97434028e7087275b40
Jerall Stalz <jerallstalz@gmail.com>
2021-12-05 11:57:56 -0500

    test 2

--- pages/works/[id].js
+++ pages/works/[id].js
@@ -1,13 +1,21 @@
+import Head from 'next/head'
+
 const PokePage = ({ name }) => {
-  return <div>pito {name ? name : ''}</div>
+  return (
+    <>
+      <Head>
+        <title>{name ? name : ''}</title>
+      </Head>
+      <div>pito {name ? name : ''}</div>
+    </>
+  )
 }
 
 export async function getStaticPaths() {
   const res = await fetch(
-    'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1117'
+    'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118'
   )
   const pokemons = await res.json()
-  console.log(pokemons)
   const paths = pokemons.results.map(path => ({
     params: {
       id: path.url.split('/')[6]
@@ -21,12 +29,20 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  console.log(params)
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
-  const pokemon = await res.json()
-  return {
-    props: {
-      name: pokemon.name
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.id}`)
+    const pokemon = await res.json()
+    return {
+      props: {
+        name: pokemon.name
+      }
+    }
+  } catch (e) {
+    console.log(e)
+    return {
+      props: {
+        name: ''
+      }
     }
   }
 }
